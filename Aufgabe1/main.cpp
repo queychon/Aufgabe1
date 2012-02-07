cabec00279b7d6925752888325ea297c2c424b1b
Michi <Qeychon@Compience.com>
2012-02-07 19:46:49 +0100

    Initial Commit

---
+++ Aufgabe1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Aufgabe1
+//
+//  Created by Michael R. on 07.02.12.
+//  Copyright (c) 2012 Compience Ltd. All rights reserved.
+//
+
+#include "systemc.h"
+
+int sc_main (int argc, char * argv[]) {
+			
+    // insert code here...
+    std::cout << "Hello, SystemC!\n";
+    sc_start();
+    return 0;
+}
+
